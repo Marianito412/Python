b2f8a84b6e0ed0de88337941f5b7acee079ef0b1
Marianito412 <Marianito412@users.noreply.github.com>
2019-12-22 15:56:52 -0600

    EpicDriver Shoul be good to go

--- PyProjects/Epic_Games_Claimer/EpicDriver.py
+++ PyProjects/Epic_Games_Claimer/EpicDriver.py
@@ -1,20 +1,34 @@
 from selenium import webdriver
 import time
-
-#email: "jmarianosoto@gmail.com"
-#password: "dfghfg"
-#rememberMe: true
-#
-
+#feel free to play around with the .sleep() times, i used this due to my slow wifi speed
+url = "https://www.epicgames.com/id/login?lang=es_ES&redirectUrl=https%3A%2F%2Fwww.epicgames.com%2Fstore%2Fes-ES%2F%3FsessionInvalidated%3Dtrue&client_id=875a3b57d3a640a6b7f9b4e883463ab4&noHostRedirect=true"
 driver = webdriver.Chrome()
-driver.get("https://www.epicgames.com/store/es-ES/?sessionInvalidated=true") #Check if that is the real url
 
-def getGame():
-    #Click obtain
-    driver.find_element_by_tag_name("").click()
-    #Click back
-    driver.find_elements_by_class_name("")
-time.sleep(3)
-games = driver.find_elements_by_class_name("CardGrid-groupWrapper_37469a6b")
-free_games = games[2].find_element_by_class_name("CardGrid-card_d639e18a")
-free_games.click()
+email = "insert your email"
+password = "you get the idea"
+
+def SignIn(driver):
+    driver.get(url)
+    time.sleep(5)
+    driver.find_element_by_id("email").send_keys("jmarianosot@gmail.com")
+    driver.find_element_by_id("password").send_keys("holasoymariano412\n")
+
+def getGame(driver):
+    obtain = driver.find_element_by_class_name("PurchaseButton-button_0fd80531")
+    obtain.click()
+    time.sleep(1)
+    purchase = driver.find_element_by_class_name("btn-primary")
+    purchase.click()
+
+def findGame(driver):
+    category = driver.find_elements_by_class_name("Discover-section_c504570a")
+    free = category[3]
+    print(free.text)
+    game = free.find_element_by_class_name("Card-root_06ca652d")
+    game.click()
+
+if __name__ == "__main__":
+    SignIn(driver)
+    time.sleep(10)
+    findGame(driver)
+    getGame(driver)
