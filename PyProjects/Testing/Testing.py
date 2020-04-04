7262266fa8b79264eb1c789bcd1577d4fdd0b7a2
Mariano Soto <jmarianosoto@gmail.com>
2020-04-03 20:10:46 -0600

    learning .json

--- PyProjects/Testing/Testing.py
+++ PyProjects/Testing/Testing.py
@@ -2,11 +2,10 @@ import json
 
 with open("test.json") as json_file:
     beta = json.load(json_file)
-beta = str(beta).replace("'", '"')
-data = json.loads(beta)
-for course in data["Classes"]:
-    print(course["name"])
-    print("_______________________________")
-with open("new_test.json", "w") as f:
-    json.dump(data, f, indent=2)
+    print(beta)
+    for course in beta:
+        print(course["name"] + "        " + )
+        print("_______________________________")
+    with open("new_test.json", "w") as f:
+        json.dump(beta, f, indent=2)
 
