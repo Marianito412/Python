9eab1b5838642260c8ebfb4a50db02e52d4a373e
Mariano Soto <jmarianosoto@gmail.com>
2020-07-28 17:58:51 -0600

    Added printMaze, colorama and a __main__

--- PyProjects/Pathfinding/BasicPathfinder.py
+++ PyProjects/Pathfinding/BasicPathfinder.py
@@ -1,11 +1,13 @@
+from colorama import Fore, Style
+
 def createMaze():
     maze=[]
-    maze.append([" ","#","X"])
-    maze.append([" "," "," "])
-    maze.append(["#","#"," "])
-    maze.append([" "," "," "])
-    maze.append([" ","#","#"])
-    maze.append([" "," ","Y"])
+    maze.append(["X","#","="])
+    maze.append(["=","=","="])
+    maze.append(["=","#","="])
+    maze.append(["=","=","="])
+    maze.append(["#","#","="])
+    maze.append(["Y","=","="])
     return maze
 
 def validMove(maze, Moves):
@@ -14,20 +16,7 @@ def validMove(maze, Moves):
             start = k
     i = start
     j = 0
-    #for move in Moves:
-    #    if move == "L":
-    #        i-=1
-    #    if move == "D":
-    #        j+=1
-    #    if move == "U":
-    #        j-=1
-    #    if move == "R":
-    #        i+=1
-#
-    #    if not(0 <= i < len(maze[0]) and 0 <= j < len(maze)):
-    #        return False
-    #    elif maze[j][i] == "#":
-    #        return False
+
     i = i + (Moves.count("R") - Moves.count("L"))
     j = j + (Moves.count("D") - Moves.count("U"))
 
@@ -35,8 +24,29 @@ def validMove(maze, Moves):
         return False
     elif maze[j][i] == "#":
         return False
-        
+
     return True
+
+def printMaze(maze, moves):
+    for k, pos in enumerate(maze[0]):
+        if pos == "X":
+            start = k
+    i = start
+    j = 0
+    for move in moves:
+        if move == "L":
+            i-=1
+        if move == "D":
+            j+=1
+        if move == "U":
+            j-=1
+        if move == "R":
+            i+=1
+        maze[j][i] = f"{Fore.RED}={Style.RESET_ALL}"
+    
+    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in maze]))
+    
+
 
 def findEnd(maze, Moves):
     for k, pos in enumerate(maze[0]):
@@ -44,20 +54,7 @@ def findEnd(maze, Moves):
             start = k
     i = start
     j = 0
-    #for move in Moves:
-    #    if move == "L":
-    #        i -=1
-    #    if move == "D":
-    #        j+=1
-    #    if move == "U":
-    #        j-=1
-    #    if move == "R":
-    #        i+=1
-#
-    #    if maze[j][i] == "Y":
-    #        print("Found: ", Moves)
-    #        return True
-    #        
+          
     i = i + (Moves.count("R") - Moves.count("L"))
     j = j + (Moves.count("D") - Moves.count("U"))
 
@@ -67,12 +64,15 @@ def findEnd(maze, Moves):
 
     return False
 
-maze = createMaze()
-q = [""]
-x = ""
-while not findEnd(maze, x):
-    x = q.pop(0)
-    for i in ["L","D","U","R"]:
-        put = x+i
-        if validMove(maze, put):
-            q.append(put)
+if __name__ == "__main__":
+    maze = createMaze()
+    q = [""]
+    x = ""
+    while not findEnd(maze, x):
+        x = q.pop(0)
+        for i in ["L","D","U","R"]:
+            put = x+i
+            if validMove(maze, put):
+                q.append(put)
+    
+    printMaze(maze, x)
