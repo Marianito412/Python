2bcabe40bd6b4dd95a4bd4e4599609133b891fae
Mariano Soto <jmarianosoto@gmail.com>
2020-09-15 23:08:22 -0600

    Implemented Class for reusability.

--- PyProjects/whatsapp-spammer.py
+++ PyProjects/whatsapp-spammer.py
@@ -1,33 +1,26 @@
 from selenium import webdriver
 
-#driver = webdriver.Chrome()
-#driver.get('https://web.whatsapp.com/')
+class WhatsappBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://web.whatsapp.com/')
+    
+    def sendMsg(self, msg):
+        msg_box = self.driver.find_element_by_xpath('//div[@dir= "ltr"][@spellcheck = "true"][@contenteditable = "true"]')
+        msg_box.send_keys(f"{msg}\n")
 
-options = webdriver.ChromeOptions()
-options.add_argument('--user-data-dir=C:/Users/Mariano/Desktop/Mariano/Projects/Workspace/Chrome profiles/User_Data_Whatsapp_spammer')
-driver = webdriver.Chrome(chrome_options=options)
-driver.get('https://web.whatsapp.com/')
+    def getChat(self, contact):
+        search = self.driver.find_element_by_class_name("copyable-text")
+        search.send_keys(name)
+        user = self.driver.find_element_by_xpath(f'//span[@title = "{name}"][@dir = "auto"]')
+        user.click()
 
-name = input('Enter the name of the target : ')
-msg = input('Enter the payload message : ')
-count = int(input('Enter the amount of shots : '))
-
-input('Scan the QR code and confirm ')
-input("Ready, standing by for confirmation ")
-
-try:
-    search = driver.find_element_by_xpath('//input[@title = "Search or start new chat"]')
-except:
-    search = driver.find_element_by_xpath('//input[@title = "Buscar o empezar un chat nuevo"]')
-search.send_keys(name)
-user = driver.find_element_by_xpath('//span[@title = "{}"][@dir = "auto"]'.format(name))
-user.click()
-
-msg_box = driver.find_element_by_xpath('//div[@dir= "ltr"][@spellcheck = "true"][@contenteditable = "true"]')
-k = 0
-for i in range(count):
-    k = k+1
-    msg_box.send_keys(msg)
-    button = driver.find_element_by_class_name('_3M-N-')
-    button.click()
-    print("Hit " + str(k) + "!")
+if __name__ == "__main__":
+    bot = WhatsappBot()
+    name = "Me"
+    msg = "Hello"
+    times = 3
+    input("Ready?")
+    bot.getChat(name)
+    for i in range(times):
+        bot.sendMsg(msg)
