60b3f23e8871d34c0f19dfdb880ac2133f8a619c
Mariano Soto <jmarianosoto@gmail.com>
2020-10-19 23:09:26 -0600

    Deleted Abandoned Projects

--- PyProjects/Telegram-Whatsapp_interface/Telegram.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import time
-from selenium import webdriver
-
-if __name__ == "__main__":
-    options = webdriver.ChromeOptions()
-    options.add_argument('--user-data-dir=C:/Users/Mariano/Desktop/Mariano/Projects/Workspace/Chrome profiles/User_Data_Telegram')
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.get('https://web.telegram.org/#/im?p=@TECCRbot')
